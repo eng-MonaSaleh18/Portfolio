8ff4df1f4cacfc7af3d430a84cc3a46479ae098b
Salehahmad897 <Salehahmad751@gmail.com>
2026-06-28 22:27:48 +0300

    edit the AdminPanelProvider file

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->authGuard('web')
             ->profile()
             ->brandName('Mona Saleh')
             ->colors([
